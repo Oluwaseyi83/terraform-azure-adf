7777eb55920637a5f662e07ca883d9b1bcddeaa8
Oluwaseyi Oyadeyi <oyasey83@gmai.com>
2024-11-19 16:05:26 -0700

    update commit

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "data_factory_id" {
   description = "Details of the created Azure Data Factory"
-  value = azurerm_data_factory.example.id
+  value = azurerm_data_factory.data_factory.id
 }  
 
 output "data_factory_name" {
   description = "Details of the created Azure Data Factory"
-  value = azurerm_data_factory.example.name
+  value = azurerm_data_factory.data_factory.name
 } 
