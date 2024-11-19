2fe1f3de5dd8ebf80d8df23051c680bf2570d9e0
Oluwaseyi Oyadeyi <oyasey83@gmai.com>
2024-11-19 15:24:35 -0700

    updated commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_data_factory" "example" {
- name                  = "test-${var.project_name}-${var.team_name}-rg-${var.environment}"
+resource "azurerm_data_factory" "data_factory" {
+ name                  = "test-${var.project_name}-${var.team_name}-adf-${var.environment}"
  location              = var.location
  resource_group_name   = var.resource_group_name
 }
